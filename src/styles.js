86a4acc44712e2125563031b0e63db6857daca26
Sohampatil20 <sohampatil947@gmail.com>
2025-02-02 13:30:52 +0530

    prod

--- src/styles.js
+++ src/styles.js
@@ -1,19 +1,17 @@
 const styles = {
-  paddingX: "px-4 sm:px-6 md:px-16",
-  paddingY: "py-4 sm:py-6 md:py-16",
-  padding: "px-4 py-6 sm:px-6 sm:py-10 md:px-16 md:py-16",
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white text-[32px] xs:text-[40px] sm:text-[50px] md:text-[60px] lg:text-[80px] leading-[40px] sm:leading-[50px] md:leading-[70px] lg:leading-[98px] mt-2",
-  
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
-    "text-[#dfd9ff] font-medium text-[14px] sm:text-[18px] md:text-[22px] lg:text-[30px] leading-[22px] sm:leading-[26px] md:leading-[30px] lg:leading-[40px]",
+    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
   sectionHeadText:
-    "text-white font-black text-[28px] xs:text-[32px] sm:text-[40px] md:text-[50px] lg:text-[60px]",
-  
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
-    "text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] text-secondary uppercase tracking-wide",
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
 
 export { styles };
